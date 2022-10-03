58db6f1ce2d4dd72446c92b58bdb2a6d8c6237b0
koyamal <74910401+koyamal@users.noreply.github.com>
2022-10-03 18:44:00 +0900

    Make UserCards display

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,33 @@
+import axios from "axios";
+import { useState } from "react";
 import { UserCard } from "./components/UserCard";
 import "./styles.css";
+import { User } from "./types/api/user";
+import { UserProfile } from "./types/userProfile";
 
-const user = {
-  id: 1,
-  name: "Tom",
-  email: "1212312@12312313",
-  address: "adfas-adfasdf-adfas"
-};
 export default function App() {
+  const [userProfile, setUserProfile] = useState<Array<UserProfile>>([]);
+  const onClickFetchUser = () => {
+    axios
+      .get<Array<User>>("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        const data = res.data.map((user) => {
+          return {
+            id: user.id,
+            name: `${user.name}(${user.username})`,
+            email: user.email,
+            address: `${user.address.city}${user.address.suite}${user.address.street}`
+          };
+        });
+        setUserProfile(data);
+      });
+  };
   return (
     <div className="App">
-      <UserCard user={user} />
+      <button onClick={onClickFetchUser}>Get Data</button>
+      {userProfile.map((user) => {
+        return <UserCard key={user.id} user={user} />;
+      })}
     </div>
   );
 }
